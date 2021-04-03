b4c07ac67916f9f1d721aea0761a2c97c9c3c19a
Ahmed khater <ahmed.m.khater89@gmail.com>
2021-04-03 04:04:41 +0200

    Initial solution

--- Querying the Document/main.c
+++ Querying the Document/main.c
@@ -5,20 +5,115 @@
 #define MAX_CHARACTERS 1005
 #define MAX_PARAGRAPHS 5
 
-char* kth_word_in_mth_sentence_of_nth_paragraph(char**** document, int k, int m, int n) {
 
+//my util variables
+typedef struct{
+    int paragraph;
+    int sentences_num;
+    int *words_num_in_sentences;
+}info;
+
+//my util functions
+void check_null(void *pointer){
+    if(pointer == NULL){
+        printf("Null pointer found\n");
+        exit(0);
+    }
+    return;
+}
+
+char* kth_word_in_mth_sentence_of_nth_paragraph(char**** document, int k, int m, int n) {
+    return document[n][m][k];
 }
 
 char** kth_sentence_in_mth_paragraph(char**** document, int k, int m) { 
-
+    return document[m][k];
 }
 
 char*** kth_paragraph(char**** document, int k) {
-
+    return document[k];
 }
 
 char**** get_document(char* text) {
-
+    char ****document = NULL;
+    char buffer[MAX_CHARACTERS+1];
+    int i, k, paragraph, sentence, word, character;
+    info meta_data[MAX_PARAGRAPHS];
+        
+    //initialize meta_data
+    for(i = 0; i < MAX_PARAGRAPHS; i++){
+        meta_data[i].paragraph = i+1;
+        meta_data[i].sentences_num = 0;
+        meta_data[i].words_num_in_sentences = NULL;
+    }
+    
+    //get number of sentences in each paragraph
+    paragraph = sentence = character = 0;
+    while(text[character] != '\0'){
+        if(text[character] == '.'){
+            meta_data[paragraph].sentences_num++;
+        }else if(text[character] == '\n'){
+            paragraph++;
+        }
+        character++;
+    }
+    //initialize word_num_in_sentences
+    for(i = 0; i < 5; i++){
+        meta_data[i].words_num_in_sentences = (int *)malloc(meta_data[i].sentences_num * sizeof(int));
+        check_null((void *)meta_data[i].words_num_in_sentences);
+        for(k = 0; k < meta_data[i].sentences_num; k++){
+            meta_data[i].words_num_in_sentences[k] = 0;
+        }
+    }
+    //get number of words in each sentence
+    paragraph = sentence = character = 0;
+    while(text[character] != '\0'){
+        if( (text[character] == ' ') || (text[character] == '.') || (text[character] == '\n') ){
+            meta_data[paragraph].words_num_in_sentences[sentence]++;
+            if(text[character] == '.'){
+                sentence++;
+            }else if(text[character] == '\n'){
+                paragraph++;
+                sentence = 0;
+            }
+        }
+        character++;
+    }
+    
+    
+    //allocate the main document pointer with first paragraph
+    document = (char ****)malloc(MAX_PARAGRAPHS * sizeof(char ***));
+    check_null((void *)document);
+    
+    //allocate paragrapgs pointers and sentences pointers
+    for(paragraph = 0; i < MAX_PARAGRAPHS && meta_data[paragraph].sentences_num != 0; paragraph++){
+        document[paragraph] = (char ***)malloc(meta_data[paragraph].sentences_num * sizeof(char **));
+        check_null((void *)document[paragraph]);
+        for(sentence = 0; sentence < meta_data[paragraph].sentences_num; sentence++){
+            document[paragraph][sentence] = (char **)malloc(meta_data[paragraph].words_num_in_sentences[sentence] * sizeof(char));
+            check_null((void *)document[paragraph][sentence]);
+        }
+    }
+        
+    //read document
+    paragraph = sentence = word = character = 0; 
+    for(i = 0; i < strlen(text); i++){
+        if(text[i] != ' ' && text[i] != '.' && text[i] != '\n'){
+            buffer[character++] = text[i]; 
+        }else if(text[i] == ' '){
+            buffer[character] = '\0';
+            document[paragraph][sentence][word++] = strdup(buffer);
+            character = 0;
+        }else if(text[i] == '.'){
+            buffer[character] = '\0';
+            document[paragraph][sentence++][word] = strdup(buffer);
+            word = character = 0;
+        }else if(text[i] == '\n'){
+            paragraph++;
+            sentence = word = character = 0;
+        }
+    }
+    return document;
 }
 
 
