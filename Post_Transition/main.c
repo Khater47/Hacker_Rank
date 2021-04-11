14d9bfc55335e95754df1560916a24061becd3da
Ahmed khater <ahmed.m.khater89@gmail.com>
2021-04-11 20:04:18 +0200

    modify main to take input from  a file

--- Post_Transition/main.c
+++ Post_Transition/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define MAX_STRING_LENGTH 6
 
 struct package
@@ -56,44 +57,50 @@ town* find_town(town* towns, int towns_count, char* name) {
 int main()
 {
 	int towns_count;
-	scanf("%d", &towns_count);
+    FILE *fp = NULL;
+    fp = fopen("/home/axe47/C C++/Hacker_Rank/Post_Transition/input.txt", "r");
+    if(fp == NULL){
+        printf("file read error\n");
+        exit(0);
+    } 
+	fscanf(fp, "%d%*c", &towns_count);
 	town* towns = malloc(sizeof(town)*towns_count);
 	for (int i = 0; i < towns_count; i++) {
 		towns[i].name = malloc(sizeof(char) * MAX_STRING_LENGTH);
-		scanf("%s", towns[i].name);
-		scanf("%d", &towns[i].offices_count);
+		fscanf(fp, "%s", towns[i].name);
+		fscanf(fp, "%d%*c", &towns[i].offices_count);
 		towns[i].offices = malloc(sizeof(post_office)*towns[i].offices_count);
 		for (int j = 0; j < towns[i].offices_count; j++) {
-			scanf("%d%d%d", &towns[i].offices[j].packages_count, &towns[i].offices[j].min_weight, &towns[i].offices[j].max_weight);
+			fscanf(fp, "%d%d%d", &towns[i].offices[j].packages_count, &towns[i].offices[j].min_weight, &towns[i].offices[j].max_weight);
 			towns[i].offices[j].packages = malloc(sizeof(package)*towns[i].offices[j].packages_count);
 			for (int k = 0; k < towns[i].offices[j].packages_count; k++) {
 				towns[i].offices[j].packages[k].id = malloc(sizeof(char) * MAX_STRING_LENGTH);
-				scanf("%s", towns[i].offices[j].packages[k].id);
-				scanf("%d", &towns[i].offices[j].packages[k].weight);
+				fscanf(fp, "%s", towns[i].offices[j].packages[k].id);
+				fscanf(fp, "%d", &towns[i].offices[j].packages[k].weight);
 			}
 		}
 	}
 	int queries;
-	scanf("%d", &queries);
+	fscanf(fp, "%d", &queries);
 	char town_name[MAX_STRING_LENGTH];
 	while (queries--) {
 		int type;
-		scanf("%d", &type);
+		fscanf(fp, "%d", &type);
 		switch (type) {
 		case 1:
-			scanf("%s", town_name);
+			fscanf(fp, "%s", town_name);
 			town* t = find_town(towns, towns_count, town_name);
 			print_all_packages(*t);
 			break;
 		case 2:
-			scanf("%s", town_name);
+			fscanf(fp, "%s", town_name);
 			town* source = find_town(towns, towns_count, town_name);
 			int source_index;
-			scanf("%d", &source_index);
-			scanf("%s", town_name);
+			fscanf(fp, "%d", &source_index);
+			fscanf(fp, "%s", town_name);
 			town* target = find_town(towns, towns_count, town_name);
 			int target_index;
-			scanf("%d", &target_index);
+			fscanf(fp, "%d", &target_index);
 			send_all_acceptable_packages(source, source_index, target, target_index);
 			break;
 		case 3:
