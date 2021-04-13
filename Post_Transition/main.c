ee423bab1116a9d3bda723cdfd8921ebc22d94ce
Ahmed khater <ahmed.m.khater89@gmail.com>
2021-04-13 04:17:27 +0200

    add function:  send_all_acceptable_packages()

--- Post_Transition/main.c
+++ Post_Transition/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_STRING_LENGTH 6
 
@@ -45,7 +46,95 @@ void print_all_packages(town t) {
     return;
 }
 
-void send_all_acceptable_packages(town* source, int source_office_index, town* target, int target_office_index) {	
+void send_all_acceptable_packages(town* source, int source_office_index, town* target, int target_office_index) {
+	int *mark = NULL;
+	int i, j = 0, package_weight, accepted_packages = 0, new_size, pos;
+	post_office *source_office = &(source->offices[source_office_index]);
+	post_office *target_office = &(target->offices[target_office_index]);
+	package *temp = NULL;
+	int target_min_weight = target_office->min_weight;
+	int target_max_weight = target_office->max_weight;
+	//create mark array
+	mark = (int *)calloc(source_office->packages_count, sizeof(int));
+	if(mark == NULL){
+		printf("Memory allocation error\n");
+		exit(1);
+	}
+	// fill mark: accepted = 1 , refused = 0;
+	for(i = 0; i < source_office->packages_count; i++){
+		package_weight = source_office->packages[i].weight;
+		if( (package_weight >= target_min_weight) && (package_weight <= target_max_weight) ){
+			mark[i] = 1;
+			accepted_packages++;
+		}else{
+			mark[i] = 0;
+		}
+	}
+	// reallocate memory to add accepted packages to target
+	new_size = target_office->packages_count + accepted_packages;
+	target_office->packages = (package *) realloc(target_office->packages, (new_size * sizeof(package)));
+	if(target_office->packages == NULL){
+		printf("Memory reallocation error\n");
+		exit(1);
+	}
+	//move accepted packages to target with the same order
+	for(i = 0; i < source_office->packages_count; i++){
+		if(mark[i] == 1){
+			//copy package of index i from source to target in pos after target_office->package_count
+			pos = ++(target_office->packages_count);
+			target_office->packages[pos].weight = source_office->packages[i].weight;
+			target_office->packages[pos].id = (char *)malloc(MAX_STRING_LENGTH * sizeof(char));
+			if(target_office->packages[pos].id == NULL){
+				printf("Memory allocation error\n");
+				exit(1);
+			}
+			strcpy(target_office->packages[pos].id, source_office->packages[i].id);
+		}
+	}
+	//modify source packages
+	new_size = source_office->packages_count - accepted_packages;
+	temp = (package *)malloc(new_size * sizeof(package));
+	if(temp == NULL){
+		printf("Memory allocation error\n");
+		exit(1);
+	}
+	//make a temp copy of the new source office packages queue
+	for(i = 0; i < source_office->packages_count; i++){
+		if(mark[i] == 0){
+			temp[j].weight = source_office->packages[i].weight;
+			temp[j].id = (char *)malloc(MAX_STRING_LENGTH * sizeof(char));
+			if(temp[j].id == NULL){
+				printf("Memory allocation error\n");
+				exit(1);
+			}
+			strcpy(temp[j].id, source_office->packages[i].id);
+			j++;
+		}
+	}
+	//cancel old queue and make the new one
+	free(source_office->packages);
+	source_office->packages = (package *)malloc(new_size * sizeof(package));
+	if(source_office->packages == NULL){
+		printf("memory allocation error\n");
+		exit(1);
+	}
+	for(i = 0; i < new_size; i++){
+		source_office->packages[i].id = (char *)malloc(MAX_STRING_LENGTH * sizeof(char));
+		if(source_office->packages[i].id == NULL){
+			printf("Memory allocation error\n");
+			exit(1);
+		}
+		strcpy(source_office->packages[i].id, temp[i].id);
+		source_office->packages[i].weight = temp[i].weight;
+	}
+	source_office->packages_count = new_size;
+	//free memory
+	free(mark);
+	for(i = 0; i < new_size; i++){
+		free(temp[i].id);
+	}
+	free(temp);
+	return;
 }
 
 town town_with_most_packages(town* towns, int towns_count) {
